19676dea5e3597541d5108cd2a390e49526e05ac
loaishar <55962133+loaishar@users.noreply.github.com>
2021-12-21 19:11:50 +0200

    f

--- main.c
+++ main.c
@@ -3,10 +3,54 @@
 #include <string.h>
 #define WORD 30
 #define TXT 1024
-
+int isSame(int *a, int *b) // compare whether two int arrays are the same
+{
+    int i;
+    for (i = 0; i < 26; i++)
+        if (a[i] != b[i])
+            return 0;
+    return 1;
+}
+char *remove_white_spaces(char str[])
+{
+    int i = 0, j = 0;
+    while (str[i])
+    {
+        if (str[i] != ' ')
+            str[j++] = str[i];
+        i++;
+    }
+    str[j] = '\0';
+    return str;
+}
+void findAnagrams2(char *str, char *word)
+{
+    int number = strlen(str);
+    char *stringa_for_rem_space = (char *)malloc(number * sizeof(char));
+    strcpy(stringa_for_rem_space, str);
+    char *stringa_without_spaces = (char *)malloc(number * sizeof(char));
+    strcpy(stringa_without_spaces, remove_white_spaces(str));
+    free(stringa_for_rem_space);
+    int *str_0 = (int *)malloc(number * sizeof(int));
+    memset(str_0, 0, number * sizeof(str_0[0]));
+    for (int i = 0; i < strlen(str); i++)
+    {
+        for (int j = 0; j < strlen(word); j++)
+        {
+            if (word[j] == str[i])
+            {
+                if (str_0[i] == 0)
+                {
+                    str_0[i]++;
+                    // i = 0;
+                }
+            }
+        }
+    }
+}
+/*****o be reviewed****/
 int main()
 {
- 
 
     //   printf("Insert word_input: MAX 8 CHARS!\n\n");
 
@@ -32,7 +76,7 @@ int main()
     /****************** end word input ************************************************/
     while ((getchar()) != '\n')
         ;
-   
+
     char string1[1024];
     //  char string1[TXT];
     scanf("%[^~]s", string1);
@@ -49,9 +93,9 @@ int main()
     //  }
     /*******end print function for test ********/
 
-    int x = word_length_geo(token);
-    printf("Gematria Sequences: ");
-    text_length_geo(string1, x);
+    // int x = word_length_geo(token);
+    // printf("Gematria Sequences: ");
+    // text_length_geo(string1, x);
     printf("\n");
 
     //  for (int i = 0; i < length_of_string2; ++i)
@@ -76,21 +120,25 @@ int main()
     /*****end reverse word*****/
     char *word1 = malloc(sizeof token);
     char *word2 = malloc(sizeof token);
-    memmove(word1, word_for_atbash_generate(token), sizeof token);
-    // printf("%s",word1);
-    // free(word1);
-    memmove(word2, word_for_atbash_generate(rev), sizeof token);
-    // printf("%s",word2);
-    // word_for_atbash_generate(rev);
+    // memmove(word1, word_for_atbash_generate(token), sizeof token);
+    //  printf("%s",word1);
+    //  free(word1);
+    // memmove(word2, word_for_atbash_generate(rev), sizeof token);
+    //  printf("%s",word2);
+    //  word_for_atbash_generate(rev);
     printf("Atbash Sequences: ");
-    hatbash(string1, word1);
+    // hatbash(string1, word1);
     free(word1);
-    hatbash(string1, word2);
+    // hatbash(string1, word2);
     free(word2);
     int xen = strlen(token);
 
     // int *ptr =
     printf("\nAnagram Sequences: ");
-    findAnagrams(string1, token);
-
+    int *p = findAnagrams(string1, token, 0);
+    int n = sizeof(p) / sizeof(int);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", p[i]);
+    }
 }
